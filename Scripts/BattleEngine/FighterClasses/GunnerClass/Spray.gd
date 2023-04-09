39882e51c2d061d5a8c659ca1f0247d762656305
beatoven9 <reeedt@gmail.com>
2023-04-08 21:29:09 -0400

    safety commit

--- Scripts/BattleEngine/FighterClasses/GunnerClass/Spray.gd
+++ Scripts/BattleEngine/FighterClasses/GunnerClass/Spray.gd
@@ -4,3 +4,4 @@ var move_name = "Spray"
 var base_power = 40
 var move_type = MOVE_TYPE.LONG_RANGE_ATTACK
 # var target_type = TARGET_TYPE.ALL_ENEMIES
+var sub_selection = ["Single Target", "All Targets"]
--- Scripts/BattleEngine/move_select_hud.gd
+++ Scripts/BattleEngine/move_select_hud.gd
@@ -2,11 +2,13 @@ extends MarginContainer
 
 
 @onready var move_selection_entry = $MarginContainer/MoveSelectionEntryBox
+@onready var move_selection_sub = $MarginContainer/MoveSelectionSubBox
+@onready var target_selection_box = $MarginContainer/MoveSelectionTargetSelectBox
 var move_list = []
 
 func prompt_for_move(available_moves, battle_state):
 	move_selection_entry.clear()
-	move_selection_entry.item_activated.connect(_on_item_selected)
+	move_selection_entry.item_activated.connect(_on_base_move_selected)
 	for move in available_moves:
 		move_selection_entry.add_item(move.move_name)
 		move_list.append(move)
@@ -17,5 +19,15 @@ func prompt_for_move(available_moves, battle_state):
 	move_selection_entry.select(0)
 	move_selection_entry.set_visible(true)
 	
-func _on_item_selected(move_index):
+func _on_base_move_selected(move_index):
 	print("Move selected: ", move_list[move_index].move_name )
+	move = move_list[move_index]
+	if move.sub_selection = false:
+		prompt_for_target()
+
+
+func prompt_for_target(move, battle_state):
+	move_selection_entry.set_visible(false)
+	var target_list = []
+	# Add enemies to this list
+	target_selection_box.set_visible(true)
