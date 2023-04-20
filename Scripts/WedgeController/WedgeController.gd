589f589c55a446a3d5edf5e3c60f2c0ee2df0a80
beatoven9 <reeedt@gmail.com>
2023-04-20 19:32:42 -0400

    WedgeController

--- Scripts/WedgeController/WedgeController.gd
+++ Scripts/WedgeController/WedgeController.gd
@@ -1,28 +1,27 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 5000.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func get_mov_vec() -> Vector2:
+	
+	var mov_vector = Vector2.ZERO
+	
+	if Input.is_action_pressed("left"):
+		mov_vector += Vector2.LEFT
+	if Input.is_action_pressed("right"):
+		mov_vector += Vector2.RIGHT
+	if Input.is_action_pressed("up"):
+		mov_vector += Vector2.UP
+	if Input.is_action_pressed("down"):
+		mov_vector += Vector2.DOWN
 
+	return mov_vector.normalized()
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	var mov_vec: Vector2 = get_mov_vec()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	velocity = mov_vec * SPEED * delta
 
 	move_and_slide()
