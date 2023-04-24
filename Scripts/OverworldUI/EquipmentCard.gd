e4f997731f8ac7d10116f3d9adda0d22722727fe
beatoven9 <reeedt@gmail.com>
2023-04-23 19:32:04 -0600

    Random UI and character sprite changes

--- Scripts/OverworldUI/EquipmentCard.gd
+++ Scripts/OverworldUI/EquipmentCard.gd
@@ -7,6 +7,23 @@ extends MarginContainer
 @onready var char_name_label: Label = $MarginContainer/HBoxContainer/MarginContainer/VBoxContainer/Label
 
 
-func _ready():
-	pass
 
+func _ready():
+	for slot in ring_slots:
+		slot.button.pressed.connect(_handle_ring_slot_press)
+
+
+func _handle_ring_slot_press():
+
+	# This isn't really the way to handle this.
+	# I should really have these modals created as scenes
+	# Which modal is shown should be handled but the individual slot
+	# not the EquipmentCard as done here.
+
+	var popup: PopupMenu = PopupMenu.new()
+	popup.set_max_size(Vector2(128, 128))
+	popup.add_item("Item 1")
+	popup.add_item("Item 2")
+	add_child(popup)
+	popup.set_visible(true)
+	popup.grab_focus()
---
+++ Scripts/OverworldUI/EquipmentSlotBase.gd
@@ -0,0 +1,10 @@
+extends MarginContainer
+
+@onready var button = $VBoxContainer/MarginContainer/Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
