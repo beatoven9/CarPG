c9f8193263fae1387d2854e27be8d75206f62300
beatoven9 <reeedt@gmail.com>
2023-04-23 20:23:52 -0600

    Added walking animations

--- Scripts/WedgeController/WedgeController.gd
+++ Scripts/WedgeController/WedgeController.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 5000.0
+var last_dir: Vector2 = Vector2.ZERO
 
 func get_mov_vec() -> Vector2:
 	
@@ -9,12 +11,21 @@ func get_mov_vec() -> Vector2:
 	
 	if Input.is_action_pressed("left"):
 		mov_vector += Vector2.LEFT
+		animated_sprite.animation = "walk_right"
+		animated_sprite.set_flip_h(true)
+
 	if Input.is_action_pressed("right"):
 		mov_vector += Vector2.RIGHT
+		animated_sprite.animation = "walk_right"
+		animated_sprite.set_flip_h(false)
+
 	if Input.is_action_pressed("up"):
 		mov_vector += Vector2.UP
+		animated_sprite.animation = "walk_right"
+
 	if Input.is_action_pressed("down"):
 		mov_vector += Vector2.DOWN
+		animated_sprite.animation = "walk_right"
 
 	return mov_vector.normalized()
 
@@ -23,5 +34,10 @@ func _physics_process(delta):
 	var mov_vec: Vector2 = get_mov_vec()
 
 	velocity = mov_vec * SPEED * delta
+
+	if mov_vec == Vector2.ZERO:
+		animated_sprite.play("idle")
+	else:
+		animated_sprite.play()
 
 	move_and_slide()
