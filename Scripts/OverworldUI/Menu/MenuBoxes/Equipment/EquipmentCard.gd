f8876223c63b30417c17e45e30e145fc8eed5eab
Laon <reeedt@gmail.com>
2023-04-27 15:40:33 -0600

    Fixed issue with equipping null instances

--- Scripts/OverworldUI/Menu/MenuBoxes/Equipment/EquipmentCard.gd
+++ Scripts/OverworldUI/Menu/MenuBoxes/Equipment/EquipmentCard.gd
@@ -8,24 +8,28 @@ extends MarginContainer
 @onready var name_label: Label = $MarginContainer/HBoxContainer/MarginContainer/VBoxContainer/Label
 
 
-
 func _ready():
 	pass
-	#for slot in ring_slots:
-	#	slot.slot_button.pressed.connect(_handle_ring_slot_press)
-
 
 func _handle_ring_slot_press():
 	pass
 
 func set_card_info(party_member: PartyMember):
-	set_card_name(party_member.name_string)
-	set_portrait(party_member.get_portrait())
-	set_class_stone(party_member.get_class_stone())
-	set_weapon(party_member.get_weapon())
-	set_ring_1(party_member.get_ring_1())
-	set_ring_2(party_member.get_ring_2())
-	set_ring_3(party_member.get_ring_3())
+	var new_name = party_member.get_name_string()
+	var new_portrait = party_member.get_portrait()
+	var new_class_stone = party_member.get_class_stone()
+	var new_weapon = party_member.get_weapon()
+	var new_ring_1 = party_member.get_ring_1()
+	var new_ring_2 = party_member.get_ring_2()
+	var new_ring_3 = party_member.get_ring_3()
+
+	set_card_name(new_name)
+	set_portrait(new_portrait)
+	set_class_stone(new_class_stone)
+	set_weapon(new_weapon)
+	set_ring_1(new_ring_1)
+	set_ring_2(new_ring_2)
+	set_ring_3(new_ring_3)
 
 func set_card_name(text: String):
 	name_label.set_text(text)
@@ -36,19 +40,24 @@ func set_portrait(new_texture: Texture2D):
 	portrait_box.set_stretch_mode(TextureRect.STRETCH_KEEP_ASPECT)
 
 func set_class_stone(new_stone: BaseClassStone):
-	class_slot.set_current_equip(new_stone)
+	if is_instance_valid(new_stone):
+		class_slot.set_current_equip(new_stone)
 
 func set_weapon(new_weapon: BaseWeapon):
-	weapon_slot.set_current_equip(new_weapon)
+	if is_instance_valid(new_weapon):
+		weapon_slot.set_current_equip(new_weapon)
 
 func set_ring_1(new_ring: BaseRing):
-	ring_slots[0].set_current_equip(new_ring)
+	if is_instance_valid(new_ring):
+		ring_slots[0].set_current_equip(new_ring)
 
 func set_ring_2(new_ring: BaseRing):
-	ring_slots[1].set_current_equip(new_ring)
+	if is_instance_valid(new_ring):
+		ring_slots[1].set_current_equip(new_ring)
 
 func set_ring_3(new_ring: BaseRing):
-	ring_slots[2].set_current_equip(new_ring)
+	if is_instance_valid(new_ring):
+		ring_slots[2].set_current_equip(new_ring)
 
 func set_ring_1_mode(new_mode):
 	pass
